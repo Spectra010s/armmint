c0796b9268a398eb60592de00b4fdbf600aef2df
Adeloye Adetayo <adeloyeadetayo8@gmail.com>
2026-09-22 16:34:25 +0100

    feat(engine): model EIP-1559 transaction fees

--- lib/server/transaction-engine/types.ts
+++ lib/server/transaction-engine/types.ts
@@ -4,6 +4,8 @@ export type TransactionRequest = {
   to: `0x${string}`;
   data: `0x${string}`;
   value?: bigint;
+  maxFeePerGas?: bigint;
+  maxPriorityFeePerGas?: bigint;
 };
 
 export type PreparedTransaction = TransactionRequest & {
